cd4f0974ad27de70c3981974d6ea2800b68966ea
thejacknives <afonso.gouveia.melo@gmail.com>
2025-11-05 02:55:55 +0000

    merda do test v3

--- movieapp/backend/app/settings.py
+++ movieapp/backend/app/settings.py
@@ -32,6 +32,22 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    }
+]
+
 ROOT_URLCONF = "app.urls"
 WSGI_APPLICATION = "app.wsgi.application"
 
