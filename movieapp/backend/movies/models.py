f809f4a991e60022a38e1372f0cbc54ee5fa258a
GeneralTT <tomas.tavares7@gmail.com>
2025-12-08 03:07:19 +0000

    models update

--- movieapp/backend/movies/models.py
+++ movieapp/backend/movies/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.db.models import Avg, Count
 
 class AppUser(models.Model):
     user_id = models.BigAutoField(primary_key=True)
@@ -18,14 +18,30 @@ class AppUser(models.Model):
 class Movie(models.Model):
     movie_id = models.BigAutoField(primary_key=True)
     title = models.CharField(max_length=512)
+    director = models.CharField(max_length=255, null=True, blank=True)
     genre = models.CharField(max_length=512)
-    description = models.CharField(max_length=512)
+    year = models.IntegerField(null=True, blank=True)
+    description = models.TextField()
+    poster_url = models.URLField(max_length=512, null=True, blank=True)
 
     class Meta:
         db_table = 'movie'
+     
 
     def __str__(self):
         return self.title
+    
+    @property
+    def average_rating(self):
+        """Calculate average rating from user ratings"""
+        result = self.ratings.aggregate(avg=Avg('score'))
+        return round(result['avg'], 2) if result['avg'] is not None else 0.0
+    
+    @property
+    def rating_count(self):
+        """Get total number of ratings"""
+        return self.ratings.count()
+
 
 
 class Rating(models.Model):
