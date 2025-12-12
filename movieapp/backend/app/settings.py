041e0df7f65c5f2050c52cb66c7275ed675e58cf
Francisco Rosendo <91336873+fsrosendo02@users.noreply.github.com>
2025-12-12 19:05:40 +0000

    Update settings.py

--- movieapp/backend/app/settings.py
+++ movieapp/backend/app/settings.py
@@ -86,6 +86,8 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://movieapp-frontend-kbj7.onrender.com",
+
 ]
 
 # Permitir o envio de cookies de sessão
@@ -95,6 +97,7 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "https://movieapp-frontend-kbj7.onrender.com",
 ]
 
 # Basic DRF default permissions (open for now)
